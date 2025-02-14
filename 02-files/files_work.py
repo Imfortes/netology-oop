c049b8ed25d1e0ca7811a62b15fb0de200005a23
Imfortes <imfortes@list.ru>
2025-02-14 23:48:21 +0300

    Исправления во 2 задании

--- 02-files/files_work.py
+++ 02-files/files_work.py
@@ -1,4 +1,5 @@
 import os
+from collections import Counter
 
 class Book:
     def __init__(self):
@@ -8,9 +9,8 @@ class Book:
         recipes = {}
         try:
             with open(file_path, 'r', encoding='utf-8') as file:
-                lines = file.readlines()
                 recipe = None
-                for line in lines:
+                for line in file:
                     line = line.strip()
                     if not line:
                         continue
@@ -33,12 +33,13 @@ class Book:
 
     def get_shop_list_by_dishes(self, dishes, person_count):
         shop_list = {} 
+        dishes_counter = Counter(dishes)
         
         for recipe in cook_book:
             if recipe in dishes:
                 for ingredient in cook_book[recipe]: 
                     name = ingredient['ingridients_name']
-                    quantity = ingredient['quantity'] * person_count 
+                    quantity = ingredient['quantity'] * person_count * dishes_counter[recipe]
                     measure = ingredient['measure']
 
                     if name in shop_list:
@@ -80,6 +81,18 @@ class File:
                     outfile.write(f'{file_info["content"]}\n')
         except Exception as e:
             print(f'Ошибка записи {e}')
+            
+    @staticmethod
+    def merge_files(file_names):
+        sorted_file_data = sorted(((name, len(open(name, encoding='utf-8').readlines())) for name in file_names), key=lambda x: x[1])
+
+        with open('./02-files/result_2.txt', 'w', encoding='utf-8') as result_file:
+            for file_name, line_count in sorted_file_data:
+                file_content = open(file_name, encoding='utf-8').read()
+                result_file.write(f'Имя файла: {file_name}\n')
+                result_file.write(f'Количество строк: {line_count}\n')
+                result_file.write(file_content + '\n')
+
 
 
 if __name__ == '__main__':
@@ -89,14 +102,15 @@ if __name__ == '__main__':
     print(f'Книга рецептов: {cook_book}')
     
     person_count = 4
-    shop_list = book.get_shop_list_by_dishes(['Омлет', 'Фахитос'], person_count)
+    shop_list = book.get_shop_list_by_dishes(['Омлет', 'Фахитос', 'Фахитос', 'Фахитос', 'Омлет', 'Омлет', 'Картошка'], person_count)
     print(f'Список покупок на {person_count} персон: {shop_list}')
     
     print()
     
     
     input_files = ['./02-files/1.txt', './02-files/2.txt', './02-files/3.txt']
-    output_file = './result.txt'
+    output_file = './02-files/result.txt'
     files_result = File.files_concat(input_files, output_file)
-    print(files_result)
+    
+    files_result_2 = File.merge_files(['./02-files/1.txt', './02-files/2.txt', './02-files/3.txt'])
     
